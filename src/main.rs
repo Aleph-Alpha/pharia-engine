866b2dfd2b0f7a7a99c24c9d65d94494a5d9c2d4
Moritz Althaus <moritz.althaus@aleph-alpha.com>
2024-06-11 14:54:49 +0200

    feat: add gracefull shutdown

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use axum::{routing::get, Router};
-use tokio::net::TcpListener;
 use std::env;
-
+use tokio::net::TcpListener;
+use tokio::signal;
 
 #[tokio::main]
 async fn main() {
@@ -15,8 +15,8 @@ async fn main() {
         .await
         .expect("Could not bind server, please check host and port"); //todo:
                                                                       //error handling
-
     axum::serve(listener, http())
+        .with_graceful_shutdown(shutdown_signal())
         .await
         .expect("Could not start server!"); //todo: error handling
 }
@@ -25,14 +25,64 @@ fn http() -> Router {
     Router::new().route("/", get(|| async { "Hello, world!" }))
 }
 
+async fn shutdown_signal() {
+    let ctrl_c = async {
+        signal::ctrl_c()
+            .await
+            .expect("failed to install Ctrl+C handler");
+    };
+
+    #[cfg(unix)]
+    let terminate = async {
+        signal::unix::signal(signal::unix::SignalKind::terminate())
+            .expect("failed to install signal handler")
+            .recv()
+            .await;
+    };
+
+    #[cfg(not(unix))]
+    let terminate = std::future::pending::<()>();
+
+    tokio::select! {
+        _ = ctrl_c => {},
+        _ = terminate => {},
+    }
+}
+
 #[cfg(test)]
 mod tests {
 
+    use super::*;
     use axum::{body::Body, http::Request};
     use http_body_util::BodyExt;
+    use libc::{getpid, kill};
+    use std::time::Duration;
+    use tokio::time::timeout;
+    use tokio_test::assert_ok;
     use tower::ServiceExt;
 
-    use super::*;
+    #[tokio::test]
+    async fn shutdown_on_sigint() {
+        let shutdown = shutdown_signal();
+        tokio::spawn(async move {
+            unsafe {
+                assert_eq!(kill(getpid(), libc::SIGINT), 0);
+            }
+        });
+        assert_ok!(timeout(Duration::from_millis(10), shutdown).await);
+    }
+
+    #[cfg(unix)]
+    #[tokio::test]
+    async fn shutdown_on_sigterm() {
+        let shutdown = shutdown_signal();
+        tokio::spawn(async {
+            unsafe {
+                assert_eq!(kill(getpid(), libc::SIGTERM), 0);
+            }
+        });
+        assert_ok!(timeout(Duration::from_millis(10), shutdown).await);
+    }
 
     #[tokio::test]
     async fn hello_world() {
