740a7533e2013173805a4e63e129680314d06a94
Daniel Tscherwinka <daniel.tscherwinka@aleph-alpha.com>
2024-06-12 16:46:04 +0200

    feat: complete text added

---
+++ src/inference.rs
@@ -0,0 +1,83 @@
+use aleph_alpha_client::{Client, How, TaskCompletion};
+use serde::{Deserialize, Serialize};
+use tokio::sync::{mpsc, oneshot};
+
+pub struct Inference {
+    sender: mpsc::Sender<InferenceMessage>,
+}
+
+impl Inference {
+    pub fn new() -> Self {
+        let (send, recv) = tokio::sync::mpsc::channel::<InferenceMessage>(1);
+        tokio::spawn(async {
+            InferenceActor::new(recv).run().await;
+        });
+        Inference { sender: send }
+    }
+    pub fn api(&self) -> InferenceApi {
+        InferenceApi {
+            sender: self.sender.clone(),
+        }
+    }
+}
+
+#[derive(Clone)]
+pub struct InferenceApi {
+    sender: mpsc::Sender<InferenceMessage>,
+}
+
+impl InferenceApi {
+    pub async fn complete_text(&mut self, params: CompleteTextParameters) -> String {
+        let (send, recv) = oneshot::channel();
+        let msg = InferenceMessage::CompleteText(params, send);
+        self.sender
+            .send(msg)
+            .await
+            .expect("all api handlers must be shutdown before actors");
+        recv.await.unwrap()
+    }
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct CompleteTextParameters {
+    pub prompt: String,
+    pub model: String,
+    pub max_tokens: u32,
+}
+
+struct InferenceActor {
+    client: Client,
+    recv: mpsc::Receiver<InferenceMessage>,
+}
+
+impl InferenceActor {
+    fn new(recv: mpsc::Receiver<InferenceMessage>) -> Self {
+        let aa_api_token = std::env::var("AA_API_TOKEN").expect("AA_API_TOKEN variable not set");
+        let client = Client::new(&aa_api_token).unwrap();
+        InferenceActor { client, recv }
+    }
+    async fn run(&mut self) {
+        while let Some(msg) = self.recv.recv().await {
+            msg.act(&self.client).await;
+        }
+    }
+}
+
+enum InferenceMessage {
+    CompleteText(CompleteTextParameters, oneshot::Sender<String>),
+}
+
+impl InferenceMessage {
+    async fn act(self, client: &Client) {
+        let _ = match self {
+            InferenceMessage::CompleteText(params, out) => {
+                let task = TaskCompletion::from_text(&params.prompt, params.max_tokens);
+                let response = client
+                    .completion(&task, &params.model, &How::default())
+                    .await
+                    .unwrap();
+                out.send(response.completion)
+            }
+        };
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,9 @@
+mod inference;
+
+use axum::extract::{Json, State};
+use axum::routing::post;
 use axum::{routing::get, Router};
+use inference::{CompleteTextParameters, Inference, InferenceApi};
 use std::env;
 use tokio::net::TcpListener;
 use tokio::signal;
@@ -8,6 +13,9 @@ async fn main() {
     // .env files are optional
     let _ = dotenvy::dotenv();
 
+    let inference = Inference::new();
+    let inference_api = inference.api();
+
     let host = env::var("HOST").expect("HOST variable not set");
     let port = env::var("PORT").expect("PORT variable not set");
     let bind = format!("{host}:{port}");
@@ -15,14 +23,24 @@ async fn main() {
         .await
         .expect("Could not bind server, please check host and port"); //todo:
                                                                       //error handling
-    axum::serve(listener, http())
+    axum::serve(listener, http(inference_api))
         .with_graceful_shutdown(shutdown_signal())
         .await
         .expect("Could not start server!"); //todo: error handling
 }
 
-fn http() -> Router {
-    Router::new().route("/", get(|| async { "Hello, world!" }))
+fn http(inference_api: InferenceApi) -> Router {
+    Router::new()
+        .route("/complete_text", post(complete_text))
+        .with_state(inference_api)
+        .route("/", get(|| async { "Hello, world!" }))
+}
+
+async fn complete_text(
+    State(mut inference_api): State<InferenceApi>,
+    Json(args): Json<CompleteTextParameters>,
+) -> String {
+    inference_api.complete_text(args).await
 }
 
 async fn shutdown_signal() {
@@ -53,18 +71,45 @@ async fn shutdown_signal() {
 mod tests {
 
     use super::*;
-    use axum::{body::Body, http::Request};
+    use axum::{
+        body::Body,
+        http::{self, Request},
+    };
     use http_body_util::BodyExt;
     use tower::ServiceExt;
 
     #[tokio::test]
     async fn hello_world() {
-        let http = http();
+        let http = http(Inference::new().api());
         let resp = http
             .oneshot(Request::builder().uri("/").body(Body::empty()).unwrap())
             .await
             .unwrap();
         let body = resp.into_body().collect().await.unwrap().to_bytes();
         assert_eq!(&body[..], b"Hello, world!");
+    }
+
+    #[tokio::test]
+    async fn complete_text() {
+        let http = http(Inference::new().api());
+        let ctp = CompleteTextParameters {
+            prompt: "An apple a day".to_owned(),
+            model: "luminous-nextgen-7b".to_owned(),
+            max_tokens: 10,
+        };
+        let resp = http
+            .oneshot(
+                Request::builder()
+                    .method(http::Method::POST)
+                    .header(http::header::CONTENT_TYPE, mime::APPLICATION_JSON.as_ref())
+                    .uri("/complete_text")
+                    .body(Body::from(serde_json::to_string(&ctp).unwrap()))
+                    .unwrap(),
+            )
+            .await
+            .unwrap();
+        assert_eq!(resp.status(), axum::http::StatusCode::OK);
+        let body = resp.into_body().collect().await.unwrap().to_bytes();
+        assert!(&body[..].starts_with(b" keeps the doctor away"));
     }
 }
