23c6a0587d25c856b9d8b4a34e6307ae8c2d1173
Moritz Althaus <moritz.althaus@aleph-alpha.com>
2024-06-20 14:57:07 +0200

    refactor: add run_greet method for runtime

--- src/skills/runtime.rs
+++ src/skills/runtime.rs
@@ -1,7 +1,32 @@
+use crate::inference::{CompleteTextParameters, InferenceApi};
+
 pub struct Runtime {}
 
 impl Runtime {
     pub fn new() -> Self {
         Self {}
+    }
+    pub async fn _run_greet(
+        &self,
+        name: String,
+        api_token: String,
+        inference_api: &mut InferenceApi,
+    ) -> String {
+        let prompt = format!(
+            "### Instruction:
+                Provide a nice greeting for the person utilizing its given name
+
+                ### Input:
+                Name: {name}
+
+                ### Response:"
+        );
+        let params = CompleteTextParameters {
+            prompt,
+            model: "luminous-nextgen-7b".to_owned(),
+            max_tokens: 10,
+        };
+        let response = inference_api.complete_text(params, api_token).await;
+        response
     }
 }
