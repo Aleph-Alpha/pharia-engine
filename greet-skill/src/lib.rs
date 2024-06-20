92ba39848ce0873081d2a894d55bcbb4ed994d5c
Daniel Tscherwinka <daniel.tscherwinka@aleph-alpha.com>
2024-06-20 12:32:26 +0200

    test: introduce skill modules

---
+++ greet-skill/src/lib.rs
@@ -0,0 +1,19 @@
+wit_bindgen::generate!({world: "skill"});
+
+struct Skill {}
+
+impl Guest for Skill {
+    fn run(name: String) -> String {
+        format!("Hello, {name}")
+    }
+}
+
+export!(Skill);
+
+#[cfg(test)]
+mod tests {
+    // use super::*;
+
+    #[test]
+    fn it_works() {}
+}
