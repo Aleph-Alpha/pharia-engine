61a7f1037d4363487f34b79557ca0df2208ff31d
Ben Brandt <benjamin.brandt@aleph-alpha.com>
2024-06-21 23:17:36 +0200

    fix: build script for new polyfill

--- build-skill.sh
+++ build-skill.sh
@@ -3,4 +3,4 @@ set -x #echo on
 
 cargo install wasm-tools
 cargo build -p greet-skill --target wasm32-wasi --release
-wasm-tools component new ./target/wasm32-wasi/release/greet_skill.wasm -o ./skills/greet_skill.wasm --adapt ./wasi_snapshot_preview1.reactor-21.0.1.wasm
+wasm-tools component new ./target/wasm32-wasi/release/greet_skill.wasm -o ./skills/greet_skill.wasm --adapt ./wasi_snapshot_preview1.reactor-22.0.0.wasm
