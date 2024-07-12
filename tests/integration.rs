9411ca6c47ace91455386a0df68800c11b4ca5d4
Jia Ho Lee <jiaho.lee@aleph-alpha.com>
2024-07-12 14:27:42 +0200

    refactor: extract kernel instance

--- tests/integration.rs
+++ tests/integration.rs
@@ -5,31 +5,53 @@ use dotenvy::dotenv;
 use pharia_kernel::{run, AppConfig};
 use reqwest::{header, Body};
 use serde_json::json;
-use tokio::sync::oneshot;
+use tokio::{sync::oneshot, task::JoinHandle};
+
+struct Kernel {
+    handle: JoinHandle<()>,
+    shutdown_trigger: oneshot::Sender<()>,
+}
+
+impl Kernel {
+    async fn new(app_config: AppConfig) -> Self {
+        let (shutdown_trigger, shutdown_capture) = oneshot::channel::<()>();
+        let shutdown_signal = async {
+            shutdown_capture.await.unwrap();
+        };
+        let wait_for_shutdown = run(app_config, shutdown_signal).await;
+        let handle = tokio::spawn(wait_for_shutdown);
+        Self {
+            handle,
+            shutdown_trigger,
+        }
+    }
+
+    async fn with_port(port: u16) -> Self {
+        let app_config = AppConfig {
+            tcp_addr: format!("127.0.0.1:{port}").parse().unwrap(),
+            inference_addr: "https://api.aleph-alpha.com".to_owned(),
+        };
+        Self::new(app_config).await
+    }
+
+    async fn shutdown(self) {
+        self.shutdown_trigger.send(()).unwrap();
+        self.handle.await.unwrap();
+    }
+}
 
 #[cfg_attr(not(feature = "test_inference"), ignore)]
 #[tokio::test]
 async fn execute_skill() {
+    const PORT: u16 = 9_000;
+    let kernel = Kernel::with_port(PORT).await;
+
     let api_token = api_token();
-
-    let app_config = AppConfig {
-        tcp_addr: "127.0.0.1:9000".to_string().parse().unwrap(),
-        inference_addr: "https://api.aleph-alpha.com".to_owned(),
-    };
-
-    let (shutdown_trigger, shutdown_capture) = oneshot::channel::<()>();
-    let shutdown_signal = async {
-        shutdown_capture.await.unwrap();
-    };
-    let shell = run(app_config, shutdown_signal).await;
-
-    let shell_handle = tokio::spawn(shell);
-
     let mut auth_value = header::HeaderValue::from_str(&format!("Bearer {api_token}")).unwrap();
     auth_value.set_sensitive(true);
     let req_client = reqwest::Client::new();
     let resp = req_client
-        .post("http://127.0.0.1:9000/execute_skill")
+        .post(format!("http://127.0.0.1:{PORT}/execute_skill"))
         .header(http::header::CONTENT_TYPE, mime::APPLICATION_JSON.as_ref())
         .header(header::AUTHORIZATION, auth_value)
         .body(Body::from(
@@ -44,8 +66,7 @@ async fn execute_skill() {
     let body = resp.text().await.unwrap();
     assert!(body.contains("Homer"));
 
-    shutdown_trigger.send(()).unwrap();
-    shell_handle.await.unwrap();
+    kernel.shutdown().await;
 }
 
 /// API Token used by tests to authenticate requests
