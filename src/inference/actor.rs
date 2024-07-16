6ef78b64e6eda6c6a6be71744083e2d56ff79aeb
Markus Klein <markus-klein@live.de>
2024-07-16 15:06:25 +0200

    test: Failing test for concurrent inference

--- src/inference/actor.rs
+++ src/inference/actor.rs
@@ -140,8 +140,13 @@ impl InferenceMessage {
 
 #[cfg(test)]
 pub mod tests {
+    use std::time::Duration;
+
     use anyhow::{anyhow, Error};
-    use tokio::{sync::mpsc, task::JoinHandle};
+    use tokio::{
+        sync::{mpsc, oneshot},
+        task::JoinHandle, time::timeout,
+    };
 
     use crate::inference::client::InferenceClient;
 
@@ -227,5 +232,107 @@ pub mod tests {
 
         // then
         assert!(result.is_ok());
+    }
+
+    /// A test double which for an inference client, which has every request pending until
+    /// explicitly told to be ready.
+    struct LatchClient {
+        /// For every call to inference we want to pop one receiver in order to wait for the answer.
+        /// It is up to the [`LatchControl`] to see to it, that these might be answered.
+        receivers: Vec<oneshot::Receiver<String>>,
+    }
+
+    impl LatchClient {
+        pub fn new(number_of_supported_requests: usize) -> (Self, LatchControl) {
+            let mut senders = Vec::new();
+            let mut receivers = Vec::new();
+            for _ in 0..number_of_supported_requests {
+                let (send, recv) = oneshot::channel();
+                senders.push(Some(send));
+                receivers.push(recv);
+            }
+            (LatchClient { receivers }, LatchControl { senders })
+        }
+    }
+
+    impl InferenceClient for LatchClient {
+        async fn complete_text(
+            &mut self,
+            _params: &CompleteTextParameters,
+            _api_token: String,
+        ) -> Result<String, Error> {
+            let recv = self.receivers.pop().expect(
+                "complete_text must not be called more often than supported by test double",
+            );
+            Ok(recv.await.unwrap())
+        }
+    }
+
+    /// Sibling of [`LatchClient`]. Used to answer the completions in any order desired by the caller
+    /// in a different thread of execution
+    struct LatchControl {
+        /// We maintain the senders as Options, so we do not need mess with the indices of the Vec
+        /// if we consume the senders. This is to support test scenarios in which we want the
+        /// completions to be answered out of order.
+        senders: Vec<Option<oneshot::Sender<String>>>,
+    }
+
+    impl LatchControl {
+        /// Answer the nth completion requested with complete text
+        pub fn answer_nth(&mut self, n: usize, completion: String) {
+            // We pop the receivers in reverse orders, so we also revert the index when answering
+            // requests.
+            let index = self.senders.len() - n - 1;
+            let send = self.senders.get_mut(index).unwrap().take().unwrap();
+            let _result = send.send(completion);
+        }
+    }
+
+    /// Dummy complete text params for test, if you do not care particular about them.
+    fn complete_text_params_dummy() -> CompleteTextParameters {
+        CompleteTextParameters {
+            prompt: "Dummy prompt".to_owned(),
+            model: "Dummy model name".to_owned(),
+            max_tokens: 128,
+        }
+    }
+
+    /// We want to ensure that the actor invokes the client multiple times concurrently instead of
+    /// only one inference request at a time.
+    #[tokio::test]
+    #[should_panic] // Inference is not concurrent yet
+    async fn concurrent_invocation_of_client() {
+        // Given
+        let (client, mut control) = LatchClient::new(2);
+        let inference = Inference::with_client(client);
+        let mut api_one = inference.api();
+        let mut api_two = inference.api();
+
+        // When
+        // This means the second call to complete_text over the api, will succeed almost immediatly,
+        // if only it is actually invoked.
+        control.answer_nth(1, "second".to_owned());
+
+        // Schedule two tasks
+        let first =
+            api_one.complete_text(complete_text_params_dummy(), "dummy api token".to_owned());
+        let second =
+            api_two.complete_text(complete_text_params_dummy(), "dummy api token".to_owned());
+
+        // Wait for the second one to be completed before answering the first one. This will block
+        // forever if inference 
+        let potential_timeout = timeout(Duration::from_secs(1), second).await;
+
+        // Then: Second task did not timeout
+        assert!(potential_timeout.is_ok());
+
+        // Free resources associated with test
+        // Second one done, now answer first task
+        control.answer_nth(0, "first".to_owned());
+        let _any_completion = first.await.unwrap();
+        // We need to drop the sender in order for `actor.run` to terminate
+        drop(api_one);
+        drop(api_two);
+        inference.wait_for_shutdown().await;
     }
 }
