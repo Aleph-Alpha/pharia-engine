e0fc0a04196616c7e2f731ea9f4a05ccf98e31ed
Ben Brandt <benjamin.brandt@aleph-alpha.com>
2024-09-07 12:56:58 +0200

    refactor: use central 0.2 WIT world for python component
    Now that componentize-py has released 0.14, we can now parse our main WIT world in Python components again.

--- build-skill-py.sh
+++ build-skill-py.sh
@@ -6,6 +6,6 @@ componentize-py -d wit/skill@unversioned/skill.wit -w skill bindings greet-py
 componentize-py -d wit/skill@unversioned/skill.wit -w skill componentize greet-py.app -o ./skills/greet-py.wasm
 wasm-tools strip ./skills/greet-py.wasm -o ./skills/greet-py.wasm
 
-componentize-py -d greet-py-v0_2/skill.wit -w skill bindings greet-py-v0_2
-componentize-py -d greet-py-v0_2/skill.wit -w skill componentize greet-py-v0_2.app -o ./skills/greet-py-v0_2.wasm
+componentize-py -d wit/skill@0.2/skill.wit -w skill bindings greet-py-v0_2
+componentize-py -d wit/skill@0.2/skill.wit -w skill componentize greet-py-v0_2.app -o ./skills/greet-py-v0_2.wasm
 wasm-tools strip ./skills/greet-py-v0_2.wasm -o ./skills/greet-py-v0_2.wasm
