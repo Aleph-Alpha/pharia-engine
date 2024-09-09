9f2394acc617169a6f8fec2b2b3a2054e768d534
Jia Ho Lee <jiaho.lee@aleph-alpha.com>
2024-09-09 14:51:11 +0200

    feat: introduce OpenTelemtry as a tracing subscriber

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,17 @@
 use anyhow::Error;
+use opentelemetry::{global, trace::TracerProvider, KeyValue};
+use opentelemetry_otlp::WithExportConfig;
+use opentelemetry_sdk::{
+    runtime,
+    trace::{BatchConfig, RandomIdGenerator, Sampler, Tracer},
+    Resource,
+};
+use opentelemetry_semantic_conventions::{
+    resource::{DEPLOYMENT_ENVIRONMENT, SERVICE_NAME, SERVICE_VERSION},
+    SCHEMA_URL,
+};
 use tokio::signal;
+use tracing_opentelemetry::OpenTelemetryLayer;
 use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt, EnvFilter};
 
 use pharia_kernel::AppConfig;
@@ -8,17 +20,59 @@ use pharia_kernel::AppConfig;
 async fn main() -> Result<(), Error> {
     drop(dotenvy::dotenv());
     let app_config = AppConfig::from_env();
-    // Set up tracing subscriber that behaves like env_logger
-    // Can switch to some other subscriber in the future
+    // Set up two tracing subscribers:
+    // * Simple env logger
+    // * OpenTelemetry
+    let tracer = init_tracer();
     tracing_subscriber::registry()
         .with(EnvFilter::from_env("PHARIA_KERNEL_LOG"))
         .with(tracing_subscriber::fmt::layer())
+        .with(OpenTelemetryLayer::new(tracer))
         .init();
 
     pharia_kernel::run(app_config, shutdown_signal())
         .await? // We booted everything up and are operational
         .await; // We shut everything down, and released all resources.
     Ok(())
+}
+
+fn init_tracer() -> Tracer {
+    let provider = opentelemetry_otlp::new_pipeline()
+        .tracing()
+        .with_trace_config(
+            opentelemetry_sdk::trace::Config::default()
+                // Customize sampling strategy
+                .with_sampler(Sampler::ParentBased(Box::new(Sampler::TraceIdRatioBased(
+                    1.0,
+                ))))
+                // If export trace to AWS X-Ray, you can use XrayIdGenerator
+                .with_id_generator(RandomIdGenerator::default())
+                .with_resource(resource()),
+        )
+        .with_batch_config(BatchConfig::default())
+        .with_exporter(
+            opentelemetry_otlp::new_exporter()
+                .tonic()
+                // Only change this if there's a conflict
+                .with_endpoint("http://localhost:4317"),
+        )
+        .install_batch(runtime::Tokio)
+        .unwrap();
+
+    global::set_tracer_provider(provider.clone());
+    provider.tracer("tracing-otel-subscriber")
+}
+
+// Create a Resource that captures information about the entity for which telemetry is recorded.
+fn resource() -> Resource {
+    Resource::from_schema_url(
+        [
+            KeyValue::new(SERVICE_NAME, env!("CARGO_PKG_NAME")),
+            KeyValue::new(SERVICE_VERSION, env!("CARGO_PKG_VERSION")),
+            KeyValue::new(DEPLOYMENT_ENVIRONMENT, "develop"),
+        ],
+        SCHEMA_URL,
+    )
 }
 
 async fn shutdown_signal() {
