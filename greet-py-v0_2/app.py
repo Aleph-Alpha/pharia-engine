844cd1e6ce41bb0cc298a1d634f998405cf8b3b7
Jia Ho Lee <jiaho.lee@aleph-alpha.com>
2024-09-24 16:29:03 +0200

    chore: fix lint issues in greet-py-v0_2

--- greet-py-v0_2/app.py
+++ greet-py-v0_2/app.py
@@ -1,6 +1,8 @@
+import json
+
 import skill.exports
 from skill.imports import csi
-import json
+
 
 class SkillHandler(skill.exports.SkillHandler):
     def run(self, input: bytes) -> bytes:
@@ -14,5 +16,5 @@ You are a helpful assistant.<|eot_id|><|start_header_id|>user<|end_header_id|>
 
 Provide a nice greeting for the person named: {input}<|eot_id|><|start_header_id|>assistant<|end_header_id|>"""
         params = csi.CompletionParams(10, None, None, None, [])
-        completion = csi.complete( "llama-3.1-8b-instruct", prompt, params)
+        completion = csi.complete("llama-3.1-8b-instruct", prompt, params)
         return json.dumps(completion.text).encode()
