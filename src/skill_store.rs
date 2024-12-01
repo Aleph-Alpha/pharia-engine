1512bc486b3d2a67fce532ea0b64cdc283029cb0
Moritz Althaus <moritz.althaus@aleph-alpha.com>
2024-12-01 16:58:06 +0100

    refactor: skill store owns skill configuration

--- src/skill_store.rs
+++ src/skill_store.rs
@@ -38,7 +38,55 @@ impl CachedSkill {
     }
 }
 
+struct SkillConfiguration {
+    known_skills: HashMap<SkillPath, String>,
+    invalid_namespaces: HashMap<String, anyhow::Error>,
+}
+
+impl SkillConfiguration {
+    fn new() -> Self {
+        Self {
+            known_skills: HashMap::new(),
+            invalid_namespaces: HashMap::new(),
+        }
+    }
+
+    fn upsert_skill(&mut self, skill: ConfiguredSkill) -> bool {
+        info!("New or changed skill: {skill}");
+        let skill_path = SkillPath::new(&skill.namespace, &skill.name);
+        self
+            .known_skills
+            .insert(skill_path.clone(), skill.tag)
+            .is_some()
+    }
+    fn remove_skill(&mut self, skill_path: &SkillPath) -> bool {
+        self.known_skills.remove(skill_path).is_some()
+    }
+    /// All configured skills, sorted by namespace and name
+    pub fn skills(&self) -> impl Iterator<Item = &SkillPath> {
+        self.known_skills.keys().sorted_by(|a, b| {
+            a.namespace
+                .cmp(&b.namespace)
+                .then_with(|| a.name.cmp(&b.name))
+        })
+    }
+    pub fn add_invalid_namespace(&mut self, namespace: String, e: anyhow::Error) {
+        self.invalid_namespaces.insert(namespace, e);
+    }
+    pub fn remove_invalid_namespace(&mut self, namespace: &str) {
+        self.invalid_namespaces.remove(namespace);
+    }
+    /// Return the registered tag for a given skill
+    fn tag(&self, skill_path: &SkillPath) -> anyhow::Result<Option<&str>> {
+        if let Some(error) = self.invalid_namespaces.get(&skill_path.namespace) {
+            return Err(anyhow!("Invalid namespace: {error}"));
+        }
+        Ok(self.known_skills.get(skill_path).map(String::as_str))
+    }
+}
+
 struct SkillStoreState {
+    configuration: SkillConfiguration,
     known_skills: HashMap<SkillPath, String>,
     cached_skills: HashMap<SkillPath, CachedSkill>,
     // key: Namespace, value: Error
@@ -49,6 +97,7 @@ struct SkillStoreState {
 impl SkillStoreState {
     pub fn new(skill_loader: SkillLoaderApi) -> Self {
         SkillStoreState {
+            configuration: SkillConfiguration::new(),
             known_skills: HashMap::new(),
             cached_skills: HashMap::new(),
             invalid_namespaces: HashMap::new(),
