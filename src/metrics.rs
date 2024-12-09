4a908e02e78a7ce020f7d61d1250e07aa2bee32a
Ben Brandt <benjamin.j.brandt@gmail.com>
2024-12-09 16:21:21 +0100

    feat: add more metrics buckets for request duration

--- src/metrics.rs
+++ src/metrics.rs
@@ -14,7 +14,7 @@ use crate::shell::ShellMetrics;
 /// - Unable to install the Prometheus recorder
 pub fn initialize_metrics(addr: impl Into<SocketAddr>) -> anyhow::Result<()> {
     const EXPONENTIAL_SECONDS: &[f64] = &[
-        0.005, 0.01, 0.025, 0.05, 0.1, 0.25, 0.5, 1.0, 2.5, 5.0, 10.0,
+        0.005, 0.01, 0.025, 0.05, 0.1, 0.25, 0.5, 1.0, 2.5, 5.0, 10.0, 25.0, 50.0, 100.0, 250.0,
     ];
 
     PrometheusBuilder::new()
