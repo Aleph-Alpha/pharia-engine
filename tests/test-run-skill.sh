1c8b8e39f2d8ebfa329ca483174611c0ab7e6786
Moritz Althaus <moritz.althaus@aleph-alpha.com>
2025-01-14 10:30:42 +0100

    ci: add test for skill execution

---
+++ tests/test-run-skill.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+TOKEN=$1
+HOST=${2-http://127.0.0.1:8081}
+
+echo "Executing skill..."
+RESPONSE_CODE=$(curl -w '%{http_code}' -s -o output.result \
+                $HOST/v1/skills/playground/haiku/run \
+                -H "Authorization: Bearer $TOKEN" \
+                -H 'Content-Type: application/json' \
+                -d '{"topic": "Oat milk"}' )
+
+cat output.result
+
+if [ "$RESPONSE_CODE" = "200" ]; then
+    exit 0
+else
+    echo "unexpected response code: RESPONSE_CODE='$RESPONSE_CODE'"
+    exit 1
+fi
