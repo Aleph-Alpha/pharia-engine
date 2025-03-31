b9fe26b45ca84b6c822b880c4ce044f528b39cc2
Ben Brandt <benjamin.j.brandt@gmail.com>
2025-03-31 12:31:14 +0200

    test: update url in integration test

--- tests/test-list-skills.sh
+++ tests/test-list-skills.sh
@@ -4,7 +4,7 @@ TOKEN=$1
 HOST=${2-http://127.0.0.1:8081}
 
 echo "Listing skills..."
-response=$(curl -sS $HOST/skills -H "Authorization: Bearer $TOKEN")
+response=$(curl -sS $HOST/v1/skills -H "Authorization: Bearer $TOKEN")
 
 if [ -z "$response" ]; then
     echo "Error: No data returned"
