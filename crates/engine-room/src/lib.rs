0d5e7236cd2f34ee8f12f59c92ab8288c767cc2c
Ben Brandt <benjamin.j.brandt@gmail.com>
2025-04-02 11:10:11 +0200

    refactor: add initial engine-room crate

---
+++ crates/engine-room/src/lib.rs
@@ -0,0 +1,6 @@
+//! The Pharia Kernel Engine Room
+//!
+//! This library hopefully contains and abstracts all of the core wasmtime knowledge needed to run
+//! skills within the Pharia Kernel. There will still be bindings and linking that happens outside
+//! of this library, but everything related to efficient execution of wasm components, regardless
+//! of the world it targets, should live here.
