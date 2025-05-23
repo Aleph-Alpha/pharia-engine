ae6d0a52db6e7bac489d9ca709e4078e49fdb150
Jia Ho Lee <jiaho.lee@aleph-alpha.com>
2025-05-23 19:27:08 +0200

    test: invoke tool from MCP server with hardcoded address

--- src/tool.rs
+++ src/tool.rs
@@ -1,4 +1,9 @@
+use futures::StreamExt;
+use reqwest::Client;
+use serde::Deserialize;
+use serde_json::Value;
 use serde_json::json;
+use std::collections::HashMap;
 
 pub struct Argument {
     pub name: String,
@@ -10,29 +15,92 @@ pub struct InvokeRequest {
     pub arguments: Vec<Argument>,
 }
 
+const MCP_SERVER_ADDRESS: &str = "http://localhost:8000/mcp";
+
+#[derive(Deserialize)]
+#[serde(tag = "type", rename_all = "snake_case")]
+enum ToolCallResponseContent {
+    Text { text: String },
+}
+
+#[derive(Deserialize)]
+#[serde(rename_all = "camelCase")]
+struct ToolCallResponseResult {
+    content: Vec<ToolCallResponseContent>,
+    // is_error: bool,
+}
+
+#[derive(Deserialize)]
+struct ToolCallResponse {
+    // jsonrpc: String,
+    // id: u64,
+    result: ToolCallResponseResult,
+}
+
 pub async fn invoke_tool(request: InvokeRequest) -> Vec<u8> {
-    // Determine the value to use based on the arguments provided.
-    // - If exactly two arguments are provided as expected, use its value.
-    // - Otherwise, response with the expectation.
-    let value = match request.arguments.len() {
-        2 if request.arguments[0].name == "a" && request.arguments[1].name == "b" => {
-            let a = String::from_utf8(request.arguments[0].value.clone())
-                .unwrap()
-                .parse::<i32>()
-                .unwrap();
-            let b = String::from_utf8(request.arguments[1].value.clone())
-                .unwrap()
-                .parse::<i32>()
-                .unwrap();
-            let sum = a + b;
-            json!(sum)
-        }
-        _ => {
-            json!(format!(
-                "Arguments a and b expected for the tool '{}'",
-                request.tool_name
-            ))
-        }
-    };
-    value.to_string().into_bytes()
+    let client = Client::new();
+    let arguments = request
+        .arguments
+        .into_iter()
+        .map(|argument| {
+            (
+                argument.name,
+                serde_json::from_slice::<Value>(&argument.value).unwrap(),
+            )
+        })
+        .collect::<HashMap<_, _>>();
+    let body = json!({
+      "jsonrpc": "2.0",
+      "id": 2,
+      "method": "tools/call",
+      "params": {
+        "name": request.tool_name,
+        "arguments": arguments
+      }
+    });
+    let response = client
+        .post(MCP_SERVER_ADDRESS)
+        // MCP server want exactly these two headers, even a wildcard is not accepted
+        .header("accept", "application/json,text/event-stream")
+        .json(&body)
+        .send()
+        .await
+        .unwrap();
+    let mut stream = response.bytes_stream();
+    let item = stream.next().await.unwrap().unwrap();
+    let item = String::from_utf8(item.to_vec()).unwrap();
+    let data = item.split("data: ").nth(1).unwrap();
+    let value = serde_json::from_str::<ToolCallResponse>(data).unwrap();
+    let ToolCallResponseContent::Text { text } = value.result.content.first().unwrap();
+    text.to_owned().into_bytes()
+}
+
+#[cfg(test)]
+mod test {
+    use test_skills::given_mcp_server;
+
+    use super::*;
+
+    #[tokio::test]
+    async fn invoke_tool_given_mcp_server() {
+        let _mcp = given_mcp_server().await;
+
+        let request = InvokeRequest {
+            tool_name: "add".to_owned(),
+            arguments: vec![
+                Argument {
+                    name: "a".to_owned(),
+                    value: json!(1).to_string().into_bytes(),
+                },
+                Argument {
+                    name: "b".to_owned(),
+                    value: json!(2).to_string().into_bytes(),
+                },
+            ],
+        };
+        let response = invoke_tool(request).await;
+        let response = String::from_utf8(response).unwrap();
+
+        assert_eq!(response, "3");
+    }
 }
