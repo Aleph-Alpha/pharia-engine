9584a3a50fc249b8448b28aa06ab37de3e875247
Moritz Althaus <moritz.althaus@aleph-alpha.com>
2025-05-25 11:51:48 +0200

    test: invoke tool returns result

--- src/tool.rs
+++ src/tool.rs
@@ -1,3 +1,4 @@
+use anyhow::anyhow;
 use futures::StreamExt;
 use reqwest::Client;
 use serde::Deserialize;
@@ -27,28 +28,33 @@ enum ToolCallResponseContent {
 #[serde(rename_all = "camelCase")]
 struct ToolCallResponseResult {
     content: Vec<ToolCallResponseContent>,
-    // is_error: bool,
+    is_error: bool,
 }
 
 #[derive(Deserialize)]
 struct ToolCallResponse {
-    // jsonrpc: String,
-    // id: u64,
     result: ToolCallResponseResult,
 }
 
-pub async fn invoke_tool(request: InvokeRequest) -> Vec<u8> {
+#[derive(Debug, thiserror::Error)]
+pub enum ToolCallError {
+    #[error("{0}")]
+    ToolCallFailed(String),
+    #[error("The tool call could not be executed, original error:{0}")]
+    Other(#[from] anyhow::Error),
+}
+
+pub async fn invoke_tool(request: InvokeRequest) -> Result<Vec<u8>, ToolCallError> {
     let client = Client::new();
     let arguments = request
         .arguments
         .into_iter()
         .map(|argument| {
-            (
-                argument.name,
-                serde_json::from_slice::<Value>(&argument.value).unwrap(),
-            )
+            serde_json::from_slice::<Value>(&argument.value).map(|value| (argument.name, value))
         })
-        .collect::<HashMap<_, _>>();
+        .collect::<Result<HashMap<_, _>, serde_json::Error>>()
+        .map_err(anyhow::Error::from)?;
+
     let body = json!({
       "jsonrpc": "2.0",
       "id": 2,
@@ -58,21 +64,50 @@ pub async fn invoke_tool(request: InvokeRequest) -> Vec<u8> {
         "arguments": arguments
       }
     });
-    let response = client
+    let mut stream = client
         .post(MCP_SERVER_ADDRESS)
         // MCP server want exactly these two headers, even a wildcard is not accepted
         .header("accept", "application/json,text/event-stream")
         .json(&body)
         .send()
         .await
-        .unwrap();
-    let mut stream = response.bytes_stream();
-    let item = stream.next().await.unwrap().unwrap();
-    let item = String::from_utf8(item.to_vec()).unwrap();
-    let data = item.split("data: ").nth(1).unwrap();
-    let value = serde_json::from_str::<ToolCallResponse>(data).unwrap();
-    let ToolCallResponseContent::Text { text } = value.result.content.first().unwrap();
-    text.to_owned().into_bytes()
+        .map_err(anyhow::Error::from)?
+        .bytes_stream();
+    let item = stream
+        .next()
+        .await
+        .ok_or(anyhow!("No item in stream"))?
+        .map(|item| String::from_utf8(item.to_vec()))
+        .map_err(anyhow::Error::from)?
+        .map_err(anyhow::Error::from)?;
+
+    let data = item
+        .split("data: ")
+        .nth(1)
+        .ok_or(anyhow!("No data in stream"))?;
+    let value = serde_json::from_str::<ToolCallResponse>(data).map_err(anyhow::Error::from)?;
+    match value.result {
+        ToolCallResponseResult {
+            content,
+            is_error: false,
+        } => {
+            let ToolCallResponseContent::Text { text } = content
+                .first()
+                .ok_or(anyhow!("No content in tool call response"))?;
+            Ok(text.to_owned().into_bytes())
+        }
+        ToolCallResponseResult {
+            content,
+            is_error: true,
+        } => {
+            // Even for errors messages, we expect a text response for each tool call. So if there is no
+            // text, the error is not a tool call failed, but rather a bad response by the MCP server.
+            let ToolCallResponseContent::Text { text } = content
+                .first()
+                .ok_or(anyhow!("No content in tool call response"))?;
+            Err(ToolCallError::ToolCallFailed(text.to_owned()))
+        }
+    }
 }
 
 #[cfg(test)]
@@ -98,9 +133,23 @@ mod test {
                 },
             ],
         };
-        let response = invoke_tool(request).await;
+        let response = invoke_tool(request).await.unwrap();
         let response = String::from_utf8(response).unwrap();
-
         assert_eq!(response, "3");
+    }
+
+    #[tokio::test]
+    async fn invoke_saboteur_tool_results_in_error() {
+        let _mcp = given_mcp_server().await;
+
+        let request = InvokeRequest {
+            tool_name: "saboteur".to_owned(),
+            arguments: vec![],
+        };
+        let response = invoke_tool(request).await.unwrap_err();
+        assert_eq!(
+            response.to_string(),
+            "Error executing tool saboteur: Out of cheese."
+        );
     }
 }
