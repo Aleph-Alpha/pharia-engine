0c3a6983c76849e5d143e771dcaced9c7b829cfa
Moritz Althaus <moritz.althaus@aleph-alpha.com>
2025-05-27 16:21:04 +0200

    refactor: move initialize to impl block of client

--- src/tool/client.rs
+++ src/tool/client.rs
@@ -20,6 +20,24 @@ impl McpClient {
     }
 }
 
+#[derive(Deserialize)]
+#[serde(tag = "type", rename_all = "snake_case")]
+enum ToolCallResponseContent {
+    Text { text: String },
+}
+
+#[derive(Deserialize)]
+#[serde(rename_all = "camelCase")]
+struct ToolCallResponseResult {
+    content: Vec<ToolCallResponseContent>,
+    is_error: bool,
+}
+
+#[derive(Deserialize)]
+struct ToolCallResponse {
+    result: ToolCallResponseResult,
+}
+
 impl ToolClient for McpClient {
     async fn invoke_tool(
         &self,
@@ -27,7 +45,7 @@ impl ToolClient for McpClient {
         mcp_address: &str,
         _tracing_context: TracingContext,
     ) -> Result<Vec<u8>, ToolError> {
-        initialize(mcp_address).await?;
+        Self::initialize(mcp_address).await?;
 
         let client = Client::new();
         let arguments = request
@@ -57,7 +75,7 @@ impl ToolClient for McpClient {
             .await
             .map_err(anyhow::Error::from)?;
 
-        let tool_response = json_rpc_response_from_http::<ToolCallResponse>(response).await?;
+        let tool_response = Self::json_rpc_response_from_http::<ToolCallResponse>(response).await?;
         match tool_response.result {
             ToolCallResponseResult {
                 content,
@@ -85,150 +103,133 @@ impl ToolClient for McpClient {
     }
 }
 
-#[derive(Deserialize)]
-#[serde(tag = "type", rename_all = "snake_case")]
-enum ToolCallResponseContent {
-    Text { text: String },
-}
-
-#[derive(Deserialize)]
-#[serde(rename_all = "camelCase")]
-struct ToolCallResponseResult {
-    content: Vec<ToolCallResponseContent>,
-    is_error: bool,
-}
-
-#[derive(Deserialize)]
-struct ToolCallResponse {
-    result: ToolCallResponseResult,
-}
-
-/// Conditionally parse the response from either an SSE stream or a JSON object.
-///
-/// If the input contains any number of JSON-RPC requests, the server MUST either return
-/// Content-Type: text/event-stream, to initiate an SSE stream, or
-/// Content-Type: application/json, to return one JSON object.
-/// The client MUST support both these cases.
-/// See: <https://modelcontextprotocol.io/specification/2025-03-26/basic/transports#sending-messages-to-the-server>
-async fn json_rpc_response_from_http<T>(response: Response) -> anyhow::Result<T>
-where
-    T: serde::de::DeserializeOwned,
-{
-    let content_type = response
-        .headers()
-        .get(header::CONTENT_TYPE)
-        .ok_or_else(|| anyhow!("No content type in response header"))?
-        .to_str()?;
-    match content_type {
-        "application/json" => {
-            let data = response.json().await?;
-            Ok(serde_json::from_value::<T>(data)?)
+impl McpClient {
+    /// Conditionally parse the response from either an SSE stream or a JSON object.
+    ///
+    /// If the input contains any number of JSON-RPC requests, the server MUST either return
+    /// Content-Type: text/event-stream, to initiate an SSE stream, or
+    /// Content-Type: application/json, to return one JSON object.
+    /// The client MUST support both these cases.
+    /// See: <https://modelcontextprotocol.io/specification/2025-03-26/basic/transports#sending-messages-to-the-server>
+    async fn json_rpc_response_from_http<T>(response: Response) -> anyhow::Result<T>
+    where
+        T: serde::de::DeserializeOwned,
+    {
+        let content_type = response
+            .headers()
+            .get(header::CONTENT_TYPE)
+            .ok_or_else(|| anyhow!("No content type in response header"))?
+            .to_str()?;
+        match content_type {
+            "application/json" => {
+                let data = response.json().await?;
+                Ok(serde_json::from_value::<T>(data)?)
+            }
+            "text/event-stream" => {
+                // We may get different type of results in the stream.
+                // A client may send different notification types before sending the result we are interested in.
+                // For now, we ignore these notification, which can include progress notifications, but also
+                // log messages. See <https://modelcontextprotocol.io/specification/2025-03-26/basic#notifications>
+                let mut stream = response.bytes_stream();
+                while let Some(Ok(item)) = stream.next().await {
+                    let item = String::from_utf8(item.to_vec())?;
+                    let data = item
+                        .split("data: ")
+                        .nth(1)
+                        .ok_or_else(|| anyhow!("No data in stream"))?;
+                    if let Ok(value) = serde_json::from_str::<T>(data) {
+                        return Ok(value);
+                    }
+                }
+                Err(anyhow!("Expected JSON-RPC response not found in stream"))
+            }
+            _ => Err(anyhow!("unexpected content type"))?,
         }
-        "text/event-stream" => {
-            // We may get different type of results in the stream.
-            // A client may send different notification types before sending the result we are interested in.
-            // For now, we ignore these notification, which can include progress notifications, but also
-            // log messages. See <https://modelcontextprotocol.io/specification/2025-03-26/basic#notifications>
-            let mut stream = response.bytes_stream();
-            while let Some(Ok(item)) = stream.next().await {
-                let item = String::from_utf8(item.to_vec())?;
-                let data = item
-                    .split("data: ")
-                    .nth(1)
-                    .ok_or_else(|| anyhow!("No data in stream"))?;
-                if let Ok(value) = serde_json::from_str::<T>(data) {
-                    return Ok(value);
+    }
+
+    /// The initialization phase MUST be the first interaction between client and server.
+    /// During this phase, the client and server:
+    /// - Establish protocol version compatibility
+    /// - Exchange and negotiate capabilities
+    /// - Share implementation details
+    ///
+    /// See: <https://modelcontextprotocol.io/specification/2025-03-26/basic/lifecycle#initialization>
+    pub async fn initialize(mcp_address: &str) -> Result<(), ToolError> {
+        #[derive(Deserialize)]
+        #[serde(rename_all = "camelCase")]
+        struct InitializeResult {
+            protocol_version: String,
+        }
+
+        #[derive(Deserialize)]
+        struct InitializeResponse {
+            result: InitializeResult,
+        }
+
+        // In the initialize request, the client MUST send a protocol version it supports.
+        // This SHOULD be the latest version supported by the client.
+        // See: <https://modelcontextprotocol.io/specification/2025-03-26/basic/lifecycle#version-negotiation>
+        const SUPPORTED_PROTOCOL_VERSIONS: &[&str] = &["2025-03-26"];
+        let body = json!({
+            "jsonrpc": "2.0",
+            "method": "initialize",
+            "id": 1,
+            "params": {
+                "protocolVersion": SUPPORTED_PROTOCOL_VERSIONS[0],
+                "capabilities": {},
+                "clientInfo": {
+                    "name": "PhariaKernel",
+                    "version": env!("CARGO_PKG_VERSION")
                 }
             }
-            Err(anyhow!("Expected JSON-RPC response not found in stream"))
+        });
+
+        let client = Client::new();
+        let response = client
+            .post(mcp_address)
+            .header("accept", "application/json,text/event-stream")
+            .json(&body)
+            .send()
+            .await
+            .map_err(Into::<anyhow::Error>::into)?;
+
+        let response = Self::json_rpc_response_from_http::<InitializeResponse>(response).await?;
+
+        // If the server supports the requested protocol version, it MUST respond with the same version.
+        // Otherwise, the server MUST respond with another protocol version it supports.
+        // This SHOULD be the latest version supported by the server.
+        // If the client does not support the version in the server's response, it SHOULD disconnect.
+        if !SUPPORTED_PROTOCOL_VERSIONS.contains(&response.result.protocol_version.as_str()) {
+            return Err(anyhow!(
+                "The proposed protocol version {} from the MCP server is not supported.",
+                response.result.protocol_version,
+            ))?;
         }
-        _ => Err(anyhow!("unexpected content type"))?,
+
+        // After successful initialization, the client MUST send an initialized notification to
+        // indicate it is ready to begin normal operations:
+        let body = json!({
+            "jsonrpc": "2.0",
+            "method": "notifications/initialized",
+        });
+
+        let response = client
+            .post(mcp_address)
+            .header("accept", "application/json,text/event-stream")
+            .json(&body)
+            .send()
+            .await
+            .map_err(anyhow::Error::from)?;
+
+        if !response.status().is_success() {
+            return Err(ToolError::Other(anyhow!(
+                "Failed to send initialized notification"
+            )));
+        }
+
+        Ok(())
     }
 }
-
-/// The initialization phase MUST be the first interaction between client and server.
-/// During this phase, the client and server:
-/// - Establish protocol version compatibility
-/// - Exchange and negotiate capabilities
-/// - Share implementation details
-///
-/// See: <https://modelcontextprotocol.io/specification/2025-03-26/basic/lifecycle#initialization>
-pub async fn initialize(mcp_address: &str) -> Result<(), ToolError> {
-    #[derive(Deserialize)]
-    #[serde(rename_all = "camelCase")]
-    struct InitializeResult {
-        protocol_version: String,
-    }
-
-    #[derive(Deserialize)]
-    struct InitializeResponse {
-        result: InitializeResult,
-    }
-
-    // In the initialize request, the client MUST send a protocol version it supports.
-    // This SHOULD be the latest version supported by the client.
-    // See: <https://modelcontextprotocol.io/specification/2025-03-26/basic/lifecycle#version-negotiation>
-    const SUPPORTED_PROTOCOL_VERSIONS: &[&str] = &["2025-03-26"];
-    let body = json!({
-        "jsonrpc": "2.0",
-        "method": "initialize",
-        "id": 1,
-        "params": {
-            "protocolVersion": SUPPORTED_PROTOCOL_VERSIONS[0],
-            "capabilities": {},
-            "clientInfo": {
-                "name": "PhariaKernel",
-                "version": env!("CARGO_PKG_VERSION")
-            }
-        }
-    });
-
-    let client = Client::new();
-    let response = client
-        .post(mcp_address)
-        .header("accept", "application/json,text/event-stream")
-        .json(&body)
-        .send()
-        .await
-        .map_err(Into::<anyhow::Error>::into)?;
-
-    let response = json_rpc_response_from_http::<InitializeResponse>(response).await?;
-
-    // If the server supports the requested protocol version, it MUST respond with the same version.
-    // Otherwise, the server MUST respond with another protocol version it supports.
-    // This SHOULD be the latest version supported by the server.
-    // If the client does not support the version in the server's response, it SHOULD disconnect.
-    if !SUPPORTED_PROTOCOL_VERSIONS.contains(&response.result.protocol_version.as_str()) {
-        return Err(anyhow!(
-            "The proposed protocol version {} from the MCP server is not supported.",
-            response.result.protocol_version,
-        ))?;
-    }
-
-    // After successful initialization, the client MUST send an initialized notification to
-    // indicate it is ready to begin normal operations:
-    let body = json!({
-        "jsonrpc": "2.0",
-        "method": "notifications/initialized",
-    });
-
-    let response = client
-        .post(mcp_address)
-        .header("accept", "application/json,text/event-stream")
-        .json(&body)
-        .send()
-        .await
-        .map_err(anyhow::Error::from)?;
-
-    if !response.status().is_success() {
-        return Err(ToolError::Other(anyhow!(
-            "Failed to send initialized notification"
-        )));
-    }
-
-    Ok(())
-}
-
 #[cfg(test)]
 pub mod tests {
     use test_skills::{given_json_mcp_server, given_sse_mcp_server};
@@ -328,6 +329,6 @@ pub mod tests {
     async fn initialize_request() {
         let mcp = given_sse_mcp_server().await;
 
-        initialize(mcp.address()).await.unwrap();
+        McpClient::initialize(mcp.address()).await.unwrap();
     }
 }
