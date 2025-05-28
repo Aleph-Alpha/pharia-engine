c2299e495a5f7bc462c6f467969c5e01a76e22b6
Moritz Althaus <moritz.althaus@aleph-alpha.com>
2025-05-28 22:43:39 +0200

    refactor: extract ToolStoreApi interface from ToolApi

--- src/tool.rs
+++ src/tool.rs
@@ -1,9 +1,9 @@
 mod actor;
 mod client;
 
-pub use self::actor::{Argument, InvokeRequest, Tool, ToolApi, ToolError};
+pub use self::actor::{Argument, InvokeRequest, Tool, ToolApi, ToolError, ToolStoreApi};
 
 #[cfg(test)]
 pub mod tests {
-    pub use super::actor::tests::ToolDouble;
+    pub use super::actor::tests::{ToolDouble, ToolStoreDouble};
 }
