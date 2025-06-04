6bc1159b94ec4675f69dd41dde11640a1b22a1c4
Moritz Althaus <moritz.althaus@aleph-alpha.com>
2025-06-04 15:09:13 +0200

    refactor: move tracing context to invocation context

--- src/csi/contextual.rs
+++ src/csi/contextual.rs
@@ -22,37 +22,31 @@ use crate::{
 pub trait ContextualCsi {
     fn explain(
         &self,
-        tracing_context: TracingContext,
         requests: Vec<ExplanationRequest>,
     ) -> impl Future<Output = Result<Vec<Explanation>, CsiError>> + Send;
 
     fn complete(
         &self,
-        tracing_context: TracingContext,
         requests: Vec<CompletionRequest>,
     ) -> impl Future<Output = anyhow::Result<Vec<Completion>>> + Send;
 
     fn completion_stream(
         &self,
-        tracing_context: TracingContext,
         request: CompletionRequest,
     ) -> impl Future<Output = mpsc::Receiver<Result<CompletionEvent, InferenceError>>> + Send;
 
     fn chat(
         &self,
-        tracing_context: TracingContext,
         requests: Vec<ChatRequest>,
     ) -> impl Future<Output = anyhow::Result<Vec<ChatResponse>>> + Send;
 
     fn chat_stream(
         &self,
-        tracing_context: TracingContext,
         request: ChatRequest,
     ) -> impl Future<Output = mpsc::Receiver<Result<ChatEvent, InferenceError>>> + Send;
 
     fn chunk(
         &self,
-        tracing_context: TracingContext,
         requests: Vec<ChunkRequest>,
     ) -> impl Future<Output = anyhow::Result<Vec<Vec<Chunk>>>> + Send;
 
@@ -61,30 +55,25 @@ pub trait ContextualCsi {
     fn select_language(
         &self,
         requests: Vec<SelectLanguageRequest>,
-        tracing_context: TracingContext,
     ) -> impl Future<Output = anyhow::Result<Vec<Option<Language>>>> + Send;
 
     fn search(
         &self,
-        tracing_context: TracingContext,
         requests: Vec<SearchRequest>,
     ) -> impl Future<Output = anyhow::Result<Vec<Vec<SearchResult>>>> + Send;
 
     fn documents(
         &self,
-        tracing_context: TracingContext,
         requests: Vec<DocumentPath>,
     ) -> impl Future<Output = anyhow::Result<Vec<Document>>> + Send;
 
     fn document_metadata(
         &self,
-        tracing_context: TracingContext,
         requests: Vec<DocumentPath>,
     ) -> impl Future<Output = anyhow::Result<Vec<Option<Value>>>> + Send;
 
     fn invoke_tool(
         &self,
-        tracing_context: TracingContext,
         requests: Vec<InvokeRequest>,
     ) -> impl Future<Output = Result<Vec<Value>, ToolError>> + Send;
 }
@@ -98,14 +87,22 @@ pub struct InvocationContext<R> {
     namespace: Namespace,
     // The authentication provided as part of the Skill invocation.
     api_token: String,
+    // Context that is used to situate certain actions in the overall context.
+    tracing_context: TracingContext,
 }
 
 impl<R> InvocationContext<R> {
-    pub fn new(raw_csi: R, namespace: Namespace, api_token: String) -> Self {
+    pub fn new(
+        raw_csi: R,
+        namespace: Namespace,
+        api_token: String,
+        tracing_context: TracingContext,
+    ) -> Self {
         Self {
             raw_csi,
             namespace,
             api_token,
+            tracing_context,
         }
     }
 }
@@ -116,99 +113,119 @@ where
 {
     fn explain(
         &self,
-        tracing_context: TracingContext,
         requests: Vec<ExplanationRequest>,
     ) -> impl Future<Output = Result<Vec<Explanation>, CsiError>> + Send {
-        self.raw_csi
-            .explain(self.api_token.clone(), tracing_context, requests)
+        self.raw_csi.explain(
+            self.api_token.clone(),
+            self.tracing_context.clone(),
+            requests,
+        )
     }
 
     fn complete(
         &self,
-        tracing_context: TracingContext,
         requests: Vec<CompletionRequest>,
     ) -> impl Future<Output = anyhow::Result<Vec<Completion>>> + Send {
-        self.raw_csi
-            .complete(self.api_token.clone(), tracing_context, requests)
+        self.raw_csi.complete(
+            self.api_token.clone(),
+            self.tracing_context.clone(),
+            requests,
+        )
     }
 
     fn completion_stream(
         &self,
-        tracing_context: TracingContext,
         request: CompletionRequest,
     ) -> impl Future<Output = mpsc::Receiver<Result<CompletionEvent, InferenceError>>> + Send {
-        self.raw_csi
-            .completion_stream(self.api_token.clone(), tracing_context, request)
+        self.raw_csi.completion_stream(
+            self.api_token.clone(),
+            self.tracing_context.clone(),
+            request,
+        )
     }
 
     fn chat(
         &self,
-        tracing_context: TracingContext,
         requests: Vec<ChatRequest>,
     ) -> impl Future<Output = anyhow::Result<Vec<ChatResponse>>> + Send {
-        self.raw_csi
-            .chat(self.api_token.clone(), tracing_context, requests)
+        self.raw_csi.chat(
+            self.api_token.clone(),
+            self.tracing_context.clone(),
+            requests,
+        )
     }
 
     fn chat_stream(
         &self,
-        tracing_context: TracingContext,
         request: ChatRequest,
     ) -> impl Future<Output = mpsc::Receiver<Result<ChatEvent, InferenceError>>> + Send {
-        self.raw_csi
-            .chat_stream(self.api_token.clone(), tracing_context, request)
+        self.raw_csi.chat_stream(
+            self.api_token.clone(),
+            self.tracing_context.clone(),
+            request,
+        )
     }
 
     fn chunk(
         &self,
-        tracing_context: TracingContext,
         requests: Vec<ChunkRequest>,
     ) -> impl Future<Output = anyhow::Result<Vec<Vec<Chunk>>>> + Send {
-        self.raw_csi
-            .chunk(self.api_token.clone(), tracing_context, requests)
+        self.raw_csi.chunk(
+            self.api_token.clone(),
+            self.tracing_context.clone(),
+            requests,
+        )
     }
 
     fn select_language(
         &self,
         requests: Vec<SelectLanguageRequest>,
-        tracing_context: TracingContext,
     ) -> impl Future<Output = anyhow::Result<Vec<Option<Language>>>> + Send {
-        self.raw_csi.select_language(requests, tracing_context)
+        self.raw_csi
+            .select_language(requests, self.tracing_context.clone())
     }
 
     fn search(
         &self,
-        tracing_context: TracingContext,
         requests: Vec<SearchRequest>,
     ) -> impl Future<Output = anyhow::Result<Vec<Vec<SearchResult>>>> + Send {
-        self.raw_csi
-            .search(self.api_token.clone(), tracing_context, requests)
+        self.raw_csi.search(
+            self.api_token.clone(),
+            self.tracing_context.clone(),
+            requests,
+        )
     }
 
     fn documents(
         &self,
-        tracing_context: TracingContext,
         requests: Vec<DocumentPath>,
     ) -> impl Future<Output = anyhow::Result<Vec<Document>>> + Send {
-        self.raw_csi
-            .documents(self.api_token.clone(), tracing_context, requests)
+        self.raw_csi.documents(
+            self.api_token.clone(),
+            self.tracing_context.clone(),
+            requests,
+        )
     }
 
     fn document_metadata(
         &self,
-        tracing_context: TracingContext,
         requests: Vec<DocumentPath>,
     ) -> impl Future<Output = anyhow::Result<Vec<Option<Value>>>> + Send {
-        self.raw_csi
-            .document_metadata(self.api_token.clone(), tracing_context, requests)
+        self.raw_csi.document_metadata(
+            self.api_token.clone(),
+            self.tracing_context.clone(),
+            requests,
+        )
     }
 
     fn invoke_tool(
         &self,
-        tracing_context: TracingContext,
         requests: Vec<InvokeRequest>,
     ) -> impl Future<Output = Result<Vec<Value>, ToolError>> + Send {
-        self.raw_csi
-            .invoke_tool(self.namespace.clone(), tracing_context, requests)
+        self.raw_csi.invoke_tool(
+            self.namespace.clone(),
+            self.tracing_context.clone(),
+            requests,
+        )
     }
 }
