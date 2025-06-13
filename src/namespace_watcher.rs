d77c3cce9f6beb695e1ad0c2dbc0c8aaae29adfb
Markus Klein <markus-klein@live.de>
2025-06-13 20:08:01 +0200

    style: group use statements

--- src/namespace_watcher.rs
+++ src/namespace_watcher.rs
@@ -2,9 +2,11 @@ mod actor;
 mod config;
 mod namespace_description;
 
-pub use self::actor::{NamespaceDescriptionLoaders, NamespaceWatcher};
-pub use self::config::{Namespace, NamespaceConfigs, Registry};
-pub use self::namespace_description::{NamespaceDescriptionLoader, SkillDescription};
+pub use self::{
+    actor::{NamespaceDescriptionLoaders, NamespaceWatcher},
+    config::{Namespace, NamespaceConfigs, Registry},
+    namespace_description::{NamespaceDescriptionLoader, SkillDescription},
+};
 
 #[cfg(test)]
 pub mod tests {
