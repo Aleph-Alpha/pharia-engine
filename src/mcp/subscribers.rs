1adec97e7f605e5efd7b8b521ded9458bec5b009
Markus Klein <markus-klein@live.de>
2025-06-15 16:44:19 +0200

    refactor: Introduce report updated tools

--- src/mcp/subscribers.rs
+++ src/mcp/subscribers.rs
@@ -1,4 +1,6 @@
-use crate::tool::ToolRuntimeSender;
+use std::{collections::HashMap, sync::Arc};
+
+use crate::tool::{QualifiedToolName, Tool, ToolRuntimeSender};
 
 #[cfg(test)]
 use double_trait::double;
@@ -7,6 +9,13 @@ use double_trait::double;
 /// by the MCP actor to send messages to receipients of tools. Outside of test code this is
 /// implemented by the [`crate::tool::ToolRuntimeApi`]
 #[cfg_attr(test, double(McpSubscriberDouble))]
-pub trait McpSubscriber {}
+pub trait McpSubscriber {
+    /// Let the subscriber know that the list of tools has been changed and report the new list
+    fn report_updated_tools(&self, tools: HashMap<QualifiedToolName, Arc<dyn Tool + Send>>);
+}
 
-impl McpSubscriber for ToolRuntimeSender {}
+impl McpSubscriber for ToolRuntimeSender {
+    fn report_updated_tools(&self, tools: HashMap<QualifiedToolName, Arc<dyn Tool + Send>>) {
+        // Do nothing for now
+    }
+}
