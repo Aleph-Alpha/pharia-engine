0204f7bcaa3621545d860070e22ab721696c71fc
Max Hammer <max.hammer@aleph-alpha.com>
2025-10-16 14:14:45 +0200

    feat: extract reasoning events in chat stream from AA inference API v1
    this is an interim step until inference API v2 is released

--- src/inference/openai.rs
+++ src/inference/openai.rs
@@ -19,7 +19,7 @@ use tokio::sync::mpsc;
 use crate::{
     authorization::Authentication,
     inference::{
-        self, InferenceError,
+        self, ChatEventV2, InferenceError,
         client::{InferenceClient, validate_chat_event, validate_chat_response},
     },
     logging::TracingContext,
@@ -589,6 +589,85 @@ pub struct ChatStreamWithReasoning {
     usage: Option<async_openai::types::CompletionUsage>,
 }
 
+/// Extracts the reasoning content from a stream of chat events.
+///
+/// With the 0.4 WIT world we introduced concept of reasoning to chat responses.
+/// However AA inference API has not stabilized this feature yet.
+/// To not block the release of the 0.4 WIT world we temporarily implement the missing parsing
+/// logic until AA Inference API v2 is publicly available.
+pub struct ReasoningExtractor {
+    is_thinking: bool,
+}
+
+impl ReasoningExtractor {
+    const START_TAG: &str = "<think>";
+    const END_TAG: &str = "</think>";
+
+    pub fn new() -> Self {
+        Self { is_thinking: false }
+    }
+
+    pub fn extract(&mut self, event: ChatEventV2) -> Vec<ChatEventV2> {
+        match (&event, self.is_thinking) {
+            (ChatEventV2::MessageAppend { content, .. }, false) => {
+                let contains_reasoning = content.starts_with(Self::START_TAG);
+                if contains_reasoning && let Some(reasoning_ends) = content.find(Self::END_TAG) {
+                    let reasoning_part = content[Self::START_TAG.len()..reasoning_ends].to_owned();
+                    let content_part = content[reasoning_ends + Self::END_TAG.len()..].to_owned();
+                    let mut events = vec![];
+                    if !reasoning_part.is_empty() {
+                        events.push(ChatEventV2::Reasoning {
+                            content: reasoning_part,
+                        });
+                    }
+                    if !content_part.is_empty() {
+                        events.push(ChatEventV2::MessageAppend {
+                            content: content_part,
+                        });
+                    }
+                    events
+                } else if contains_reasoning {
+                    self.is_thinking = true;
+                    let reasoning_part = content[Self::START_TAG.len()..].to_owned();
+                    if reasoning_part.is_empty() {
+                        vec![]
+                    } else {
+                        vec![ChatEventV2::Reasoning {
+                            content: reasoning_part,
+                        }]
+                    }
+                } else {
+                    vec![event]
+                }
+            }
+            (ChatEventV2::MessageAppend { content, .. }, true) => {
+                if let Some(reasoning_ends) = content.find(Self::END_TAG) {
+                    self.is_thinking = false;
+                    let reasoning_part = content[..reasoning_ends].to_owned();
+                    let content_part = content[reasoning_ends + Self::END_TAG.len()..].to_owned();
+                    let mut events = vec![];
+
+                    if !reasoning_part.is_empty() {
+                        events.push(ChatEventV2::Reasoning {
+                            content: reasoning_part,
+                        });
+                    }
+                    if !content_part.is_empty() {
+                        events.push(ChatEventV2::MessageAppend {
+                            content: content_part,
+                        });
+                    }
+                    events
+                } else {
+                    vec![ChatEventV2::Reasoning {
+                        content: content.to_owned(),
+                    }]
+                }
+            }
+            _ => vec![event],
+        }
+    }
+}
 impl inference::ChatEvent {
     /// Convert a [`ChatStreamWithReasoning`] into a vector of [`inference::ChatEvent`].
     ///
@@ -962,89 +1041,6 @@ pub mod tests {
         fn end() -> Self {
             Self::MessageEnd {
                 finish_reason: inference::FinishReason::Stop,
-            }
-        }
-    }
-    /// Extracts the reasoning content from a stream of chat events.
-    ///
-    /// With the 0.4 WIT world we introduced concept of reasoning to chat responses.
-    /// However AA inference API has not stabilized this feature yet.
-    /// To not block the release of the 0.4 WIT world we temporarily implement the missing parsing
-    /// logic until AA Inference API v2 is publicly available.
-    struct ReasoningExtractor {
-        is_thinking: bool,
-    }
-
-    impl ReasoningExtractor {
-        const START_TAG: &str = "<think>";
-        const END_TAG: &str = "</think>";
-
-        fn new() -> Self {
-            Self { is_thinking: false }
-        }
-
-        fn extract(&mut self, event: ChatEventV2) -> Vec<ChatEventV2> {
-            match (&event, self.is_thinking) {
-                (ChatEventV2::MessageAppend { content, .. }, false) => {
-                    let contains_reasoning = content.starts_with(Self::START_TAG);
-                    if contains_reasoning && let Some(reasoning_ends) = content.find(Self::END_TAG)
-                    {
-                        let reasoning_part =
-                            content[Self::START_TAG.len()..reasoning_ends].to_owned();
-                        let content_part =
-                            content[reasoning_ends + Self::END_TAG.len()..].to_owned();
-                        let mut events = vec![];
-                        if !reasoning_part.is_empty() {
-                            events.push(ChatEventV2::Reasoning {
-                                content: reasoning_part,
-                            });
-                        }
-                        if !content_part.is_empty() {
-                            events.push(ChatEventV2::MessageAppend {
-                                content: content_part,
-                            });
-                        }
-                        events
-                    } else if contains_reasoning {
-                        self.is_thinking = true;
-                        let reasoning_part = content[Self::START_TAG.len()..].to_owned();
-                        if reasoning_part.is_empty() {
-                            vec![]
-                        } else {
-                            vec![ChatEventV2::Reasoning {
-                                content: reasoning_part,
-                            }]
-                        }
-                    } else {
-                        vec![event]
-                    }
-                }
-                (ChatEventV2::MessageAppend { content, .. }, true) => {
-                    if let Some(reasoning_ends) = content.find(Self::END_TAG) {
-                        self.is_thinking = false;
-                        let reasoning_part = content[..reasoning_ends].to_owned();
-                        let content_part =
-                            content[reasoning_ends + Self::END_TAG.len()..].to_owned();
-                        let mut events = vec![];
-
-                        if !reasoning_part.is_empty() {
-                            events.push(ChatEventV2::Reasoning {
-                                content: reasoning_part,
-                            });
-                        }
-                        if !content_part.is_empty() {
-                            events.push(ChatEventV2::MessageAppend {
-                                content: content_part,
-                            });
-                        }
-                        events
-                    } else {
-                        vec![ChatEventV2::Reasoning {
-                            content: content.to_owned(),
-                        }]
-                    }
-                }
-                _ => vec![event],
             }
         }
     }
